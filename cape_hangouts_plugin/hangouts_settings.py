7efd38298981fe4b8500316ff506144a651dd275
Mike Sheldon <mike@bloomsbury.ai>
2018-08-13 15:10:38 +0100

    Remove unused variable

--- cape_hangouts_plugin/hangouts_settings.py
+++ cape_hangouts_plugin/hangouts_settings.py
@@ -19,4 +19,3 @@ URL_BASE = '/hangouts'
 hangouts_event_endpoints = Blueprint('hangouts_event_endpoints')
 hangouts_verification = os.getenv('CAPE_HANGOUTS_VERIFICATION', 'REPLACEME')
 
-THIS_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__)))
